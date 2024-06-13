b95050cc92f94e71daa8f4a3039d7b70a20aed77
Luca <63828912+lucabarcelo@users.noreply.github.com>
2024-06-12 21:17:44 -0400

    edits load data.R

--- App/load data.R
+++ App/load data.R
@@ -6,10 +6,10 @@
 # shiny_token <- gs_auth() # authenticate w/ your desired Google identity here
 # saveRDS(shiny_token, "shiny_app_token.rds")
 # files <- gs_ls()
-# sheet_key <- files[files$sheet_title=="MIAS Nitrate",]$sheet_key
+# sheet_key <- files[files$sheet_title=="",]$sheet_key
 # put the value of sheet_key below!
 
 #readRDS("../shiny_app_token.rds")
-#sheet_key <- "1h0nrOlNymJ8nLQfd8SFFVr6hVt6D4LtX_pZOdnwD8Io"
-sheet_key <- "1zfRJT2hcbJlTiLEPqre1o6Sq8Fkmu1ErZ--eujadOFg"
+#sheet_key <- ""
+sheet_key <- ""
 sheet <- gs_key(sheet_key)
